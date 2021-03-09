40ad24d81793069da310acdaae73a74ed05a4486
Cleber Ramires Machado <cleber.ramires.machado@gmail.com>
2021-03-08 23:36:23 -0300

    Heap Sort

--- src/main/java/edu/ifrs/vvs/HeapSort.java
+++ src/main/java/edu/ifrs/vvs/HeapSort.java
@@ -1,12 +1,12 @@
 /**
  * @License
  * Copyright 2021 Heap Sort Application
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
  * http://www.apache.org/licenses/LICENSE-2.0
+ */
+/**
  * https://pt.wikipedia.org/wiki/Heapsort
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,6 +24,9 @@ import java.util.Scanner;
  */
 public class HeapSort {
 
+    /**
+     * Scanner.
+     */
     private Scanner scanner = new Scanner(System.in);
 
     /**
